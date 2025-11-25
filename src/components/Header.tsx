e8926c08f49bf43a41e5523781221dfd432de1ec
MaxAgui <manuelmaximo19@gmail.com>
2025-11-25 17:06:34 -0500

    refactor: header style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,40 +1,58 @@
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Header() {
   return (
-    <View
-      style={{
-        width: "100%",
-        paddingHorizontal: 16,
-        paddingVertical: 12,
-        backgroundColor: "white",
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-      }}
-    >
+    <View style={styles.container}>
       <Image
-        source={require("../../assets/images/logo_rimac.png")}
-        style={{ width: 100, height: 30, resizeMode: "contain" }}
+        source={require("@/assets/images/logo_rimac.png")}
+        style={styles.logo}
       />
 
       <TouchableOpacity
-        style={{
-          flexDirection: "row",
-          alignItems: "center",
-        }}
+        style={styles.phoneButton}
         onPress={() => {
         }}
       >
         <Image
-          source={require("../../assets/icons/icon-phone.png")}
-          style={{ width: 100, height: 30, resizeMode: "contain" }}
+          source={require("@/assets/icons/icon-phone.png")}
+          style={styles.phoneIcon}
         />
 
-        <Text style={{ marginLeft: 6, fontSize: 16, fontWeight: "600" }}>
+        <Text style={styles.phoneText}>
           (01) 411 6001
         </Text>
       </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    paddingHorizontal: 24,
+    paddingVertical: 12,
+    backgroundColor: "white",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  logo: {
+    width: 65.05,
+    height: 32,
+    resizeMode: "contain",
+  },
+  phoneButton: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  phoneIcon: {
+    width: 20,
+    height: 20,
+    resizeMode: "contain",
+  },
+  phoneText: {
+    marginLeft: 8,
+    fontSize: 16,
+    fontWeight: "700",
+  },
+});
