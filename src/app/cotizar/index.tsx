2c142d001b3681b6675e2d1171fce3f60db53331
MaxAgui <manuelmaximo19@gmail.com>
2025-11-25 10:57:20 -0500

    refactor: add src

--- src/app/cotizar/index.tsx
+++ src/app/cotizar/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 
-const index = () => {
+const Cotizar = () => {
   return (
     <View>
-      <Text>index</Text>
+      <Text>Seguro salud Flexible</Text>
     </View>
   )
 }
 
-export default index
+export default Cotizar
